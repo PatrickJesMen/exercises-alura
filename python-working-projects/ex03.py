88977024056f922ea9b36bbc361d0ca0818467db
PatrickJesMen <patrickjesusmen@outlook.com>
2026-04-25 16:27:10 -0300

    Add initial implementations for tip calculator, password generator, and number guessing game

---
+++ python-working-projects/ex03.py
@@ -0,0 +1,17 @@
+# Objective:
+# - Create a random number generator, and ask the user to guess the number.
+
+# Imports
+from random import randint
+from os import system
+from time import sleep
+
+# Functions
+def number_generator():
+    
+
+def start_program():
+    # Introduction
+    print("Generating number...")
+    sleep(1s)
+    print("Generated! Guess the number!")
